033a92b9448058cb79a6f09ccaf85d7485017e16
Bishal Shrestha <bishalshrestha33@gmail.com>
2016-10-08 15:29:48 +0545

    Add Game#game_over_message method with specs

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -1,8 +1,8 @@
 require "tic_tac_toe/version"
-require_relative "./tic_tac_toe/cell.rb"
-require_relative "./tic_tac_toe/player.rb"
-require_relative "./tic_tac_toe/board.rb"
-require_relative "./tic_tac_toe/core_extensions.rb"
+
+%w(cell player board core_extensions game).each do |file|
+  require_relative "./tic_tac_toe/#{file}.rb"
+end
 
 module TicTacToe
   # Your code goes here...
