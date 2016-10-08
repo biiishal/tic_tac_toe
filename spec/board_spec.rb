5cd8789e74c21ea9e707a11e21e7f70389f10a4d
Bishal Shrestha <bishalshrestha33@gmail.com>
2016-10-08 11:28:08 +0545

    Add additional tests for Board#game_over method

--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -2,6 +2,10 @@ require "spec_helper"
 
 module TicTacToe
   describe Board do
+    TestCell = Struct.new(:value)
+    let(:x_cell) { TestCell.new("X") }
+    let(:y_cell) { TestCell.new("Y") }
+    let(:empty) { TestCell.new("") }
 
     context "#initialize" do
       it "initializes the board with a grid" do
@@ -65,6 +69,56 @@ module TicTacToe
         allow(board).to receive(:winner?) { false }
         allow(board).to receive(:draw?) { false }
         expect(board.game_over).to be_falsey
+      end
+
+      it "returns :winner when row has objects with values that are all the same" do
+        grid = [
+          [x_cell, x_cell, x_cell],
+          [y_cell, y_cell, x_cell],
+          [x_cell, y_cell, y_cell]
+        ]
+        board = Board.new(grid: grid)
+        expect(board.game_over).to eq :winner
+      end
+
+      it "returns :winner when column has objects with values that are all the same" do
+        grid = [
+          [x_cell, y_cell, x_cell],
+          [x_cell, y_cell, y_cell],
+          [x_cell, x_cell, y_cell]
+        ]
+        board = Board.new(grid: grid)
+        expect(board.game_over).to eq :winner
+      end
+
+      it "returns :winner when diagonal has objects with values that are all the same" do
+        grid = [
+          [x_cell, y_cell, y_cell],
+          [x_cell, x_cell, y_cell],
+          [y_cell, y_cell, x_cell]
+        ]
+        board = Board.new(grid: grid)
+        expect(board.game_over).to_eq :winner
+      end
+
+      it "returns :draw when all spaces in the board are not empty" do
+        grid = [
+          [x_cell, y_cell, y_cell],
+          [x_cell, y_cell, x_cell],
+          [y_cell, x_cell, x_cell]
+        ]
+        board = Board.new(grid: grid)
+        expect(board.game_over).to eq :draw
+      end
+
+      it "returns false when there is no winner or draw" do
+        grid = [
+          [x_cell, '', ''],
+          [y_cell, '', ''],
+          [x_cell, '', '']
+        ]
+        board = Board.new
+        expect(board.game_over)
       end
     end
 
